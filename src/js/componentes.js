b184b57c72be0a316ee7be9f4b70d50ebcef16d2
Eutiquio <eu7i22@gmail.com>
2021-02-03 12:22:59 -0600

    Primer commit

---
+++ src/js/componentes.js
@@ -0,0 +1,10 @@
+import '../css/componentes.css';
+
+
+export const saludar = (nombre)=>{
+    console.log('Creado una etiqueta h1');
+    const h1 = document.createElement('h1');
+    h1.innerHTML = `Hola, ${nombre}`;
+   
+    document.body.append(h1);
+   };
